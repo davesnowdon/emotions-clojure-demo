36f962c7140014a90ab554ed221a47a0c128ada6
Dave Snowdon <dave@davesnowdon.com>
2014-01-28 08:02:22 +0000

    updated project

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject emotions-demo "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Demonstrate use of core.aync and emotions framework with NAO robot"
+  :url "https://github.com/davesnowdon/emotions-clojure-demo"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [emotions "0.1.0"]
+                 [naojure "0.2.0"]])
