78a31d8d7a200d4e511d7eb94af297ad03d6fec6
Dave Snowdon <dave@davesnowdon.com>
2014-02-13 08:19:05 +0000

    snapshot with project file changes

--- project.clj
+++ project.clj
@@ -3,9 +3,48 @@
   :url "https://github.com/davesnowdon/emotions-clojure-demo"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :source-paths ["src" "src/main/clojure"]
-  :test-paths ["test" "src/test/clojure"]
+
+  :main emotions-demo.core
+
+  :source-paths ["src" "src/main/clj"]
+  :test-paths ["test" "src/test/clj"]
+
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [emotions "0.1.3"]
-                 [naojure "0.2.1"]]
-  :main emotions-demo.core)
+                 [naojure "0.2.2"]
+
+                 [org.clojure/clojurescript "0.0-2156"]
+                 [org.clojure/tools.reader "0.8.3"]
+                 [prismatic/dommy "0.1.2"]
+                 [om "0.3.6"]]
+  :plugins [[lein-pdo "0.1.1"]
+            [lein-cljsbuild "1.0.2"]
+            [jarohen/lein-frodo "0.2.11"]]
+
+  :frodo/config-resource "config/demo.edn"
+
+  :aliases {"dev" ["pdo" "cljsbuild" "auto" "dev," "frodo"]
+            "prod" ["pdo" "cljsbuild" "once" "release," "frodo"]}
+
+  :resource-paths ["resources" "target/resources"]
+
+  :cljsbuild {
+     :builds [{:id "dev"
+               :source-paths ["src/main/cljs"]
+               :compiler {
+                 :output-to "target/resources/js/demo.js"
+                 :output-dir "target/resources/out"
+                 :optimizations :none
+                 :source-map true}}
+
+              {:id "release"
+               :source-paths ["src/cljs"]
+               :compiler {
+                :output-to "target/resources/js/demo.js"
+                :optimizations :advanced
+                :pretty-print false
+                :preamble ["react/react.min.js"]
+                :externs ["react/externs/react.js"]}}
+                ]}
+
+)
---
+++ src/main/cljs/emotions_demo/client.cljs
@@ -0,0 +1,69 @@
+(ns emotions-demo.client
+  (:require-macros [cljs.core.async.macros :refer [go go-loop]])
+  (:refer-clojure :exclude [chars])
+  (:require
+    [cljs.core.async :refer [<! >! put! close! timeout]]
+    [chord.client :refer [ws-ch]]
+    [om.core :as om :include-macros true]
+    [om.dom :as dom :include-macros true]))
+
+(enable-console-print!)
+
+(defn save-message [e owner]
+  (om/set-state! owner :message-to-send (.. e -target -value)))
+
+(defn send-message [ws e owner]
+  (put! ws (om/get-state owner :message-to-send))
+  (om/set-state! owner :message-to-send "")
+  false)
+
+(defn the-sender [{:keys [ws]} owner]
+  (reify
+    om/IInitState
+    (init-state [_] {:message-to-send ""})
+    om/IRender
+    (render [_]
+      (dom/form #js {:onSubmit #(send-message ws % owner)}
+      (dom/h3 nil "Send message to server")
+      (dom/input
+          #js {:type "text"
+               :ref "text-field"
+               :value (om/get-state owner :message-to-send)
+               :onChange #(save-message % owner)})))))
+
+(defn message [{:keys [message]} owner]
+  (om/component
+    (dom/li nil message)))
+
+
+(defn bind-msgs [{:keys [ws messages]}]
+  (go-loop []
+    (when-let [msg (<! ws)]
+      (om/update! messages conj msg)
+      (recur))))
+
+(defn message-list [data owner]
+  (reify
+    om/IWillMount
+    (will-mount [_]
+      (bind-msgs data))
+    om/IRender
+    (render [_]
+      (dom/div nil
+        (dom/h3 nil "Messages received from server")
+        (dom/ul nil
+          (om/build-all message (data :messages)))))))
+
+(go
+  (let [ws (<! (ws-ch "ws://localhost:3000/ws"))
+        app-state (atom {:ws ws :messages [{:message "None Yet!"}]})]
+        (om/root
+          app-state
+          (fn [app owner]
+            (reify
+              om/IRender
+              (render [_]
+                (dom/div nil
+                  (om/build the-sender app)
+                  (om/build message-list app)))))
+          (.getElementById js/document "app"))))
